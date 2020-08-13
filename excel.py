86301eabc25376cc10777969818a36e28f9ebd4f
KyEn <55014000+kyleKKKKK@users.noreply.github.com>
2020-08-13 21:14:20 +0800

    Update excel.py

--- excel.py
+++ excel.py
@@ -1,17 +1,37 @@
 from openpyxl import load_workbook
-wb = load_workbook('test.xlsx')
+wb = load_workbook('化學成績登記.xlsx')
 
 ws = wb.active
 
 while(True):
-    number = input("號碼: ")
-    grades = input("分數: ")
-    if number == 'end' and grades == 'end':
-        break
-    else:
-        ws['A'+ number] = grades
-        print('\n')
-ws.delete_cols(1,1)
-wb.save('test.xlsx')
+    order = input("enter? plus? initial? \n")   
+    if order == "initial":  #initial excel  
+        for i in range(1,40):
+            ws['A' + str(i)] = 0
 
+    elif order == "close":  #end program
+        break
+    
+    while order == "enter": #enter the score
+        number = input("號碼: ")
+        grades = input("分數: ")
+        if number == "end" and grades == "end":
+            break
+        else:
+            ws['A'+ number] = int(grades)
+            print('\n')
+            
+    if order == "plus": #plus score to the number
+        score = int(input("要加的分數: "))
+        while(True):
+            plusnumber = input("要加分的人: ")
+            if(plusnumber == "end"):
+                break
+            else:
+                ws['A' + plusnumber].value += score
+
+                
+
+
+wb.save('化學成績登記.xlsx')
 
